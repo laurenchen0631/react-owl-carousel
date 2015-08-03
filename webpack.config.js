491e67d899b7ead014873d85cb69cec1483b4da5
Xhriman <seal789ie@gmail.com>
2015-08-03 14:48:40 +0800

    update lib

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
 			},
 			{
 				test: /\.css$/,
-				loader: "css-loader",
+				loader: "style-loader!css-loader",
 				include: path.resolve(process.cwd(), "components")
 			},
 		]
